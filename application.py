dc085112a014066c15c693e0365140e3f495805e
Mukheem Mohammed <49473863+Mukheem1603@users.noreply.github.com>
2020-07-01 13:06:19 +0530

    Update application.py

--- application.py
+++ application.py
@@ -8,17 +8,13 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 app = Flask(__name__)
-# Check for environment variable
+
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
-# res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "qQB2Uiw9My1Hrlov379BQ", "isbns": "9781632168146"})
-# print(res.json())
-# Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
